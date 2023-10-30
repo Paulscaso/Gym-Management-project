cda66a5abd5f1290751df6f0539bdf96707610f4
paulscaso <paulkimotho193@gmail.com>
2023-10-30 10:21:53 +0300

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Gym App
+
+This is gym management system that will help me learn all the 
+
+#### License
+
+MIT
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+	install_requires = f.read().strip().split("\n")
+
+# get version from __version__ variable in gym_app/__init__.py
+from gym_app import __version__ as version
+
+setup(
+	name="gym_app",
+	version=version,
+	description="This is gym management system that will help me learn all the ",
+	author="Paul Kimotho",
+	author_email="paulkimotho2@gmail.com",
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=install_requires
+)
